74dd69ba697389559be14ab9ed60c71daab7f3e4
Cristofer <crismaverdeveloper@gmail.com>
2025-01-17 07:23:40 -0600

    Eliminar archivos ignorados del repositorio

--- src/Config.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'https://raw.githubusercontent.com/crismaver1993/UNIR/refs/heads/main/web';
-
-export const MOVIE_DETAIL_ENDPOINT = '/detail/movie';
-export const MOVIES_LIST_ENDPOINT = '/movies-list';
